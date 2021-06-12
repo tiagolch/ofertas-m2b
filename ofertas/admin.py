1ebe7ce9681e05f198cee7236bcb87dfc33767a4
m2b-tiagochaves <tiagodevch@gmail.com>
2021-06-12 02:00:27 -0300

    Criado models e admin

--- ofertas/admin.py
+++ ofertas/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Cities, States, Apps, Svas, Ofertas
 
-# Register your models here.
+
+@admin.register(States)
+class StatesAdmin(admin.ModelAdmin):
+    list_display = ['uf']
+
+
+@admin.register(Cities)
+class CitiesAdmin(admin.ModelAdmin):
+    list_display = ['cities']
+
+
+@admin.register(Apps)
+class AppsAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Svas)
+class SvasAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Ofertas)
+class OfertasAdmin(admin.ModelAdmin):
+    list_display = ['promocao']
+    list_filter = ['state', 'is_special', 'price']
--- ofertas/models.py
+++ ofertas/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class States(models.Model):
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.uf
+
+    class Meta:
+        verbose_name_plural = "States"
+
+
+class Cities(models.Model):
+    cities = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cities
+
+    class Meta:
+        verbose_name_plural = "Cities"
+
+
+class Apps(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Apps"
+
+
+class Svas(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Svas"
+
+
+class Ofertas(models.Model):
+    promocao = models.CharField(max_length=200, unique=True)
+    state = models.ManyToManyField(States, blank=True, null=True)
+    cities = models.ManyToManyField(Cities, blank=True, null=True)
+    is_special = models.BooleanField(default=False)
+    special_text = models.CharField(max_length=200, default=None, blank=True, null=True)
+    speed = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    apps = models.ManyToManyField(Apps, blank=True)
+    svas = models.ManyToManyField(Svas, blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount = models.DecimalField(max_digits=8, decimal_places=2)
+    tax = models.DecimalField(max_digits=8, decimal_places=2)
+    installments = models.PositiveIntegerField(default=0)
+    discount_description = models.CharField(max_length=200)
+    payment_code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.promocao
+
+    class Meta:
+        verbose_name_plural = "Ofertas"
